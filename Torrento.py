e7668c3ee47a6c72b23e3c4b74866d1d6b59fc05
shyamkap <63502065+shyamkap@users.noreply.github.com>
2020-04-12 22:26:30 +0530

    Add files via upload

---
+++ Torrento.py
@@ -0,0 +1,346 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np # library to handle data in a vectorized manner
+
+import pandas as pd # library for data analsysis
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
+import json # library to handle JSON files
+
+get_ipython().system("conda install -c conda-forge geopy --yes # uncomment this line if you haven't completed the Foursquare API lab")
+from geopy.geocoders import Nominatim # convert an address into latitude and longitude values
+
+import requests # library to handle requests
+from pandas.io.json import json_normalize # tranform JSON file into a pandas dataframe
+
+# Matplotlib and associated plotting modules
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+
+# import k-means from clustering stage
+from sklearn.cluster import KMeans
+
+#!conda install -c conda-forge folium=0.5.0 --yes # uncomment this line if you haven't completed the Foursquare API lab
+import folium # map rendering library
+
+get_ipython().system('conda install -c conda-forge bs4 --yes')
+#!conda install -c conda-forge python-lxml --yes
+get_ipython().system('conda install -c anaconda lxml --yes')
+
+from bs4 import BeautifulSoup
+
+
+# In[46]:
+
+
+#to read table from wikipedia page
+
+res = requests.get("https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M")
+soup = BeautifulSoup(res.content,'lxml')
+table = soup.find_all('table')[0] 
+df = pd.read_html(str(table))
+#print(df[0].to_json(orient='records'))
+data=df[0]
+
+
+# In[47]:
+
+
+data=data[data.Borough !='Not assigned']   #toremove all Not assigned values
+data['Neighborhood']=data['Neighborhood'].replace('/',',',regex=True)   #to replcae / with ,
+
+data.head()
+
+
+# In[27]:
+
+
+data.shape   #shape of the data will be displayed
+
+
+# #                                            SECOND PART OF THE ASSIGNMENT   
+
+# In[48]:
+
+
+#reading data from csv file
+lat_data=pd.read_csv('http://cocl.us/Geospatial_data')
+lat_data.head()
+
+
+# In[49]:
+
+
+#change index columns for merging
+lat_data.set_index("Postal Code", inplace=True)
+
+data.set_index("Postal code", inplace=True)
+
+
+# In[67]:
+
+
+#merging two dataframes based on index 
+
+merged = data.merge(lat_data, left_index=True, right_index=True, how='inner')
+merged.reset_index(inplace=True)
+merged=merged.rename({'index':'Postal Code'},axis=1)
+merged
+
+
+
+# In[68]:
+
+
+#change all Borough to Toronto that contains toronto word in it
+        
+value='Toronto'
+for i in merged.index.values:
+    if(value in merged.loc[i,'Borough']):
+        merged.loc[i,'Borough']=value      
+
+merged
+
+
+# In[70]:
+
+
+#fetching only toronto neighbours
+
+toronto_data = merged[merged['Borough'] == 'Toronto'].reset_index(drop=True)
+toronto_data
+
+
+# In[72]:
+
+
+address = 'Toronto'
+
+geolocator = Nominatim(user_agent="ny_explorer")
+location = geolocator.geocode(address)
+latitude = location.latitude
+longitude = location.longitude
+print('The geograpical coordinate of Manhattan are {}, {}.'.format(latitude, longitude))
+
+
+# In[74]:
+
+
+# create map of toronto using latitude and longitude values
+map_toronto = folium.Map(location=[latitude, longitude], zoom_start=11)
+
+# add markers to map
+for lat, lng, label in zip(toronto_data['Latitude'], toronto_data['Longitude'], toronto_data['Neighborhood']):
+    label = folium.Popup(label, parse_html=True)
+    folium.CircleMarker(
+        [lat, lng],
+        radius=5,
+        popup=label,
+        color='blue',
+        fill=True,
+        fill_color='#3186cc',
+        fill_opacity=0.7,
+        parse_html=False).add_to(map_toronto)  
+    
+map_toronto
+
+
+# In[75]:
+
+
+CLIENT_ID = '4UQKF1VNK1U5TOKBML3SFX2FWQGVEWDUK00TL34FA0TDV31W' # your Foursquare ID
+CLIENT_SECRET = 'DKF0KWXPWV1IC5NDCASYUNB5F5MLUXATG2QP02IHL05SMKQX' # your Foursquare Secret
+VERSION = '20180605' # Foursquare API version
+
+print('Your credentails:')
+print('CLIENT_ID: ' + CLIENT_ID)
+print('CLIENT_SECRET:' + CLIENT_SECRET)
+
+
+# In[76]:
+
+
+LIMIT = 100
+radius = 500
+#to find all the venues near all the neighbours of toronto within radius 500
+def getNearbyVenues(names, latitudes, longitudes, radius=500):
+    
+    venues_list=[]
+    for name, lat, lng in zip(names, latitudes, longitudes):
+        print(name)
+            
+        # create the API request URL
+        url = 'https://api.foursquare.com/v2/venues/explore?&client_id={}&client_secret={}&v={}&ll={},{}&radius={}&limit={}'.format(
+            CLIENT_ID, 
+            CLIENT_SECRET, 
+            VERSION, 
+            lat, 
+            lng, 
+            radius, 
+            LIMIT)
+            
+        # make the GET request
+        results = requests.get(url).json()["response"]['groups'][0]['items']
+        
+        # return only relevant information for each nearby venue
+        venues_list.append([(
+            name, 
+            lat, 
+            lng, 
+            v['venue']['name'], 
+            v['venue']['location']['lat'], 
+            v['venue']['location']['lng'],  
+            v['venue']['categories'][0]['name']) for v in results])
+
+    nearby_venues = pd.DataFrame([item for venue_list in venues_list for item in venue_list])
+    nearby_venues.columns = ['Neighborhood', 
+                  'Neighborhood Latitude', 
+                  'Neighborhood Longitude', 
+                  'Venue', 
+                  'Venue Latitude', 
+                  'Venue Longitude', 
+                  'Venue Category']
+    
+    return(nearby_venues)
+
+
+# In[77]:
+
+
+toronto_venues = getNearbyVenues(names=toronto_data['Neighborhood'],
+                                   latitudes=toronto_data['Latitude'],
+                                   longitudes=toronto_data['Longitude']
+                                  )
+
+
+# In[81]:
+
+
+toronto_venues.groupby('Neighborhood').count()
+
+
+# In[82]:
+
+
+# one hot encoding
+toronto_onehot = pd.get_dummies(toronto_venues[['Venue Category']], prefix="", prefix_sep="")
+
+# add neighborhood column back to dataframe
+toronto_onehot['Neighborhood'] = toronto_venues['Neighborhood'] 
+
+# move neighborhood column to the first column
+fixed_columns = [toronto_onehot.columns[-1]] + list(toronto_onehot.columns[:-1])
+toronto_onehot = toronto_onehot[fixed_columns]
+
+toronto_onehot.head()
+
+
+# In[84]:
+
+
+toronto_grouped = toronto_onehot.groupby('Neighborhood').mean().reset_index()
+toronto_grouped
+
+
+# In[85]:
+
+
+def return_most_common_venues(row, num_top_venues):
+    row_categories = row.iloc[1:]
+    row_categories_sorted = row_categories.sort_values(ascending=False)
+    
+    return row_categories_sorted.index.values[0:num_top_venues]
+
+
+# In[87]:
+
+
+num_top_venues = 10
+
+indicators = ['st', 'nd', 'rd']
+
+# create columns according to number of top venues
+columns = ['Neighborhood']
+for ind in np.arange(num_top_venues):
+    try:
+        columns.append('{}{} Most Common Venue'.format(ind+1, indicators[ind]))
+    except:
+        columns.append('{}th Most Common Venue'.format(ind+1))
+
+# create a new dataframe
+neighborhoods_venues_sorted = pd.DataFrame(columns=columns)
+neighborhoods_venues_sorted['Neighborhood'] = toronto_grouped['Neighborhood']
+
+for ind in np.arange(toronto_grouped.shape[0]):
+    neighborhoods_venues_sorted.iloc[ind, 1:] = return_most_common_venues(toronto_grouped.iloc[ind, :], num_top_venues)
+
+neighborhoods_venues_sorted.head()
+
+
+# In[88]:
+
+
+# set number of clusters
+kclusters = 5
+
+toronto_grouped_clustering = toronto_grouped.drop('Neighborhood', 1)
+
+# run k-means clustering
+kmeans = KMeans(n_clusters=kclusters, random_state=0).fit(toronto_grouped_clustering)
+
+# check cluster labels generated for each row in the dataframe
+kmeans.labels_[0:10] 
+
+
+# In[89]:
+
+
+# add clustering labels
+neighborhoods_venues_sorted.insert(0, 'Cluster Labels', kmeans.labels_)
+
+toronto_merged = toronto_data
+
+# merge toronto_grouped with toronto_data to add latitude/longitude for each neighborhood
+toronto_merged = toronto_merged.join(neighborhoods_venues_sorted.set_index('Neighborhood'), on='Neighborhood')
+
+toronto_merged.head() # check the last columns!
+
+
+# In[90]:
+
+
+# create map
+map_clusters = folium.Map(location=[latitude, longitude], zoom_start=11)
+
+# set color scheme for the clusters
+x = np.arange(kclusters)
+ys = [i + x + (i*x)**2 for i in range(kclusters)]
+colors_array = cm.rainbow(np.linspace(0, 1, len(ys)))
+rainbow = [colors.rgb2hex(i) for i in colors_array]
+
+# add markers to the map
+markers_colors = []
+for lat, lon, poi, cluster in zip(toronto_merged['Latitude'], toronto_merged['Longitude'], toronto_merged['Neighborhood'], toronto_merged['Cluster Labels']):
+    label = folium.Popup(str(poi) + ' Cluster ' + str(cluster), parse_html=True)
+    folium.CircleMarker(
+        [lat, lon],
+        radius=5,
+        popup=label,
+        color=rainbow[cluster-1],
+        fill=True,
+        fill_color=rainbow[cluster-1],
+        fill_opacity=0.7).add_to(map_clusters)
+       
+map_clusters
+
+
+# In[ ]:
+
+
+
+
